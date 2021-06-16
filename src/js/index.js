7151b632290b0591bf27609a602a87b4a448f42a
Ravil Akzamutdinov <Akzamutdinov1997@mail.ru>
2021-06-16 22:12:24 +0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+
+import "../js/components/brands";
+
+console.log("Works!");
+
+
+import "../scss";
+import "../scss/header.scss";
+import "../scss/service.scss";
+import "../scss/modal.scss";
+import "../scss/menu.scss";
+import "../scss/brands.scss";
+import "../scss/technics.scss";
+import "../scss/price.scss";
+import "../scss/footer.scss";
+import "./components/service.js";
+import "./components/modal.js";
+import "./components/brands.js";
+import "./components/technics.js";
+import "./components/price.js";
+
+console.log("Works!");
